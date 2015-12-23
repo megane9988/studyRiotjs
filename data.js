1fe618ec471453ca8efbc1b62e97aae95e781f82
megane9988 <info@m-g-n.me>
2015-12-23 22:08:58 +0900

    first commit

---
+++ data.js
@@ -0,0 +1,10 @@
+riot.mount('newslist', {
+  title: 'What is New',
+  items: [
+    {title : 'demo1', date:'2015/12/23' , url : 'demo.html'},
+    {title : 'demo2', date:'2015/12/23' , url : 'demo2.html'},
+    {title : 'demo3', date:'2015/12/23' , url : 'demo3.html'},
+    {title : 'demo4', date:'2015/12/23' , url : 'demo4.html'},
+    {title : 'demo5', date:'2015/12/23' , url : 'demo5.html'}
+  ]
+})
---
+++ newslist.tag
@@ -0,0 +1,6 @@
+<newslist>
+  <h3>{ opts.title }</h3>
+  <ul>
+    <li each={ opts.items }>{ date } : <a href="{ url }">{ title }</a></li>
+  </ul>
+</newslist>
